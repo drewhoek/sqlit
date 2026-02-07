437c1137a849ab1b08cbe454b17d2124647a7dd8
Tyler Abbott <tabbott85@gmail.com>
2026-02-07 17:00:37 -0600

    add ibm db2 for i into the new connection UI

--- sqlit/domains/connections/domain/config.py
+++ sqlit/domains/connections/domain/config.py
@@ -16,6 +16,7 @@ class DatabaseType(str, Enum):
     D1 = "d1"
     DUCKDB = "duckdb"
     DB2 = "db2"
+    DB2I = "db2i"
     FIREBIRD = "firebird"
     FLIGHT = "flight"
     HANA = "hana"
@@ -50,6 +51,7 @@ DATABASE_TYPE_DISPLAY_ORDER: list[DatabaseType] = [
     DatabaseType.ORACLE,
     DatabaseType.ORACLE_LEGACY,
     DatabaseType.DB2,
+    DatabaseType.DB2I,
     DatabaseType.HANA,
     DatabaseType.TERADATA,
     DatabaseType.SNOWFLAKE,
